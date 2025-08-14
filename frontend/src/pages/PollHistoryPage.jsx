2d188d995ff1955a2ba20da7bf35439fc5e035b3
Priyasharma297 <146953675+Priyasharma297@users.noreply.github.com>
2025-08-14 17:46:11 +0530

    backend url fixed

--- frontend/src/pages/PollHistoryPage.jsx
+++ frontend/src/pages/PollHistoryPage.jsx
@@ -8,7 +8,7 @@ const PollHistoryPage = () => {
     useEffect(() => {
         const fetchHistory = async () => {
             try {
-                const res = await axios.get("http://localhost:5000/api/polls/history");
+                const res = await axios.get("https://polling-system-backend-t17a.onrender.com");
                 setPollHistory(res.data); // Make sure this matches your backend response
             } catch (err) {
                 console.error("Failed to fetch poll history", err);
